d9c3d475d0ddf7d473205fadf5c65dc570427772
Davis W. Frank <dwfrank@gmail.com>
2020-11-04 13:17:49 -0800

    moves GA key to config; embeds GA script tags in layout

--- config.rb
+++ config.rb
@@ -83,7 +83,7 @@ set :linkedin_username, 'daviswfrank'
 # Replace 'nil' with your Disqus shortname, eg. 'example'
 #set :disqus_shortname, nil
 # Replace 'nil' with your Google Analytics key, eg. 'XX-XXXXXXXX-X'
-set :google_analytics, true
+set :google_analytics, "G-SNXCW3490N"
 
 ###
 # Environment settings
