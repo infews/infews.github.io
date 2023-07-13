3e3e174c809864c0a0e1b1d235023604e9b43c1b
Davis W. Frank <dwfrank@gmail.com>
2023-07-13 08:06:08 -0700

    Removes dead helper code

--- helpers/article_teaser_helpers.rb
+++ helpers/article_teaser_helpers.rb
@@ -1,8 +1,5 @@
 module ArticleTeaserHelpers
   def teaser_for(article)
-    teaser = article.data.teaser
-    return "" unless teaser
-
-    article.data.teaser
+    article.data.teaser || ""
   end
 end
