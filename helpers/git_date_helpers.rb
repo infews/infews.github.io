3df810353d3cba77986fc58606567f3eeb8f4e8e
Davis W. Frank <dwfrank@gmail.com>
2023-08-14 12:16:45 -0700

    Fixes bugs with new files

--- helpers/git_date_helpers.rb
+++ helpers/git_date_helpers.rb
@@ -1,10 +1,5 @@
 require "time"
 module GitDateHelpers
-  def updated_at(source_path)
-    relative_file_path_in_git = source_path.split("/").drop_while { |e| e != "source" }.join("/")
-    Time.parse(`git log -n 1 --pretty=format:%cI -- #{relative_file_path_in_git}`)
-  end
-
   def updated_date(source_path)
     time = updated_at(source_path)
     time.strftime("%Y.%m.%d")
@@ -16,14 +11,27 @@ module GitDateHelpers
   end
 
   def created_date_dashed(source_path)
-    path = git_path(source_path)
-    time = Time.parse(` git log --pretty=format:"%ad" --date=short --diff-filter=A -- #{path}`)
-    time.strftime("%Y-%m-%d")
+    time = `git log --pretty=format:"%ad" --date=short --diff-filter=A -- #{git_path(source_path)}`
+    if time.empty?
+      time = Time.now
+      time.strftime("%Y-%m-%d")
+    else
+      time
+    end
   end
 
   private
 
   def git_path(full_path)
     full_path.split("/").drop_while { |e| e != "source" }.join("/")
+  end
+
+  def updated_at(source_path)
+    updated_time = `git log -n 1 --pretty=format:%cI -- #{git_path(source_path)}`
+    if updated_time.empty?
+      Time.new
+    else
+      Time.parse(updated_time)
+    end
   end
 end
--- spec/helpers/git_date_helpers_spec.rb
+++ spec/helpers/git_date_helpers_spec.rb
@@ -8,15 +8,24 @@ RSpec.describe GitDateHelpers do
   let(:helpers) { Helpers.new }
   let(:full_path) { "/Users/dwfrank/workspace/infews.github.io/source/articles/2011-03-24-kids-teachable-moments.html.md" }
 
-  describe "#updated_at" do
-    it "returns a Time object that's the last time the file was updated in git" do
-      expect(helpers.updated_at(full_path)).to eq(Time.parse("2023-07-17 14:32:18.000000000 -0700"))
-    end
-  end
-
   describe "#updated_date" do
     it "returns a String with just the date" do
       expect(helpers.updated_date(full_path)).to eq("2023.07.17")
+    end
+
+    context "when a file is not yet in git" do
+      let(:full_path) { "/Users/dwfrank/workspace/infews.github.io/source/newfile.md" }
+      before do
+        system("touch #{full_path}")
+      end
+
+      after do
+        system("rm #{full_path}")
+      end
+
+      it "returns the current date" do
+        expect(helpers.updated_date(full_path)).to eq(Time.now.strftime("%Y.%m.%d"))
+      end
     end
   end
 
@@ -29,6 +38,20 @@ RSpec.describe GitDateHelpers do
   describe "#created_at_dashed" do
     it "returns a String with just the date in a dashed format" do
       expect(helpers.created_date_dashed(full_path)).to eq("2020-09-21")
+    end
+    context "when a file is not yet in git" do
+      let(:full_path) { "/Users/dwfrank/workspace/infews.github.io/source/newfile.md" }
+      before do
+        system("touch #{full_path}")
+      end
+
+      after do
+        system("rm #{full_path}")
+      end
+
+      it "returns the current date" do
+        expect(helpers.created_date_dashed(full_path)).to eq(Time.now.strftime("%Y-%m-%d"))
+      end
     end
   end
 end
