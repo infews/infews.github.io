941d4be85067dda16b9a289b0aaa8d6250df0799
Davis W. Frank <dwfrank@gmail.com>
2023-12-20 17:40:16 -0800

    Publishes

---
+++ docs/js/share-e3488ce2.js
@@ -0,0 +1,1 @@
+enableSharing=function(){navigator.share&&document.querySelectorAll(".share").forEach(function(t){t.addEventListener("click",function(){t.style.display="block;",navigator.share({url:window.location.href,title:"Sharing - "+t.getAttribute("article"),text:"I found this article at DWF's Journal and thought you might like it."})})})},enableSharing();
